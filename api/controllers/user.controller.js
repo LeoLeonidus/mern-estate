cff9eec05fe7fcbb4a8dc50df8517b0a3e28e41d
Massimo <verzoni.massimo@gmail.com>
2023-11-10 18:50:48 +0100

    API route & controller : created get user listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs'
 
@@ -53,3 +54,16 @@ export const deleteUser = async (req,res,next) => {
         next(error);
     };
 };
+
+export const getUserListing = async (req,res,next) => {
+    if ( req.user.id != req.params.id ) {
+        return next(errorHandler(401,"You can only access your listing"));
+    };
+
+    try {
+        const listings = await Listing.find({ userRef: req.params.id});
+        res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+}
